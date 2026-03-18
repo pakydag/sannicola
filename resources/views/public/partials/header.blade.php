54713264323bdc344818b8ca784a6ef326df98d0
paky <dagnellopasquale@gmail.com>
2026-03-18 16:41:11 +0100

    Descrizione delle modifiche grafiche

--- resources/views/public/partials/header.blade.php
+++ resources/views/public/partials/header.blade.php
@@ -30,7 +30,7 @@
 
                 <!-- Navigation Links -->
                 <nav class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <a href="{{ route('public.home') }}" class="inline-flex items-center px-1 pt-1 border-b-2 {{ request()->routeIs('public.home') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }} text-sm font-medium leading-5 transition duration-150 ease-in-out">
+                    <a href="{{ route('public.home') }}" class="inline-flex items-center px-1 pt-1 border-b-2 {{ request()->routeIs('public.home') ? 'border-red-800 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }} text-sm font-medium leading-5 transition duration-150 ease-in-out">
                         Home
                     </a>
                     
