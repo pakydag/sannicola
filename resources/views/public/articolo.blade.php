d322ede4baa978d0086d75f91f4bd3f3ad405c2c
paky <dagnellopasquale@gmail.com>
2026-04-22 11:48:57 +0200

    Modifica il layout della sezione articolo: aggiorna lo sfondo e allinea il breadcrumb

--- resources/views/public/articolo.blade.php
+++ resources/views/public/articolo.blade.php
@@ -4,9 +4,9 @@
 
 @section('content')
 <div class="relative bg-gray-50 h-64 md:h-32 flex items-end bg-cover bg-center bg-fixed" style="background-image: url('http://185.56.170.138/file-manager/preview?disk=public&path=section.jpg&v=1776845090');">
-    <div class="mx-auto max-w-7xl w-full bg-cover bg-center bg-fixed xl:p-14 p-8" style="background-image: url('http://185.56.170.138/file-manager/preview?disk=public&path=trulli.png&v=1776848703');">
+    <div class="mx-auto max-w-7xl w-full bg-white text-center">
         <!-- Breadcrumb / Back link -->
-        <nav class=" flex mt-8 mb-8 items-center text-sm font-medium text-gray-500">
+        <nav class=" flex mt-6 items-center justify-center text-sm font-medium text-gray-500">
             <a href="{{ route('public.home') }}" class="hover:text-gray-900">Home</a>
             <svg class="h-5 w-5 shrink-0 text-gray-400 mx-1" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" /></svg>
             @if($articolo->section && $articolo->section->visibile)
