3376db2422f5a9f6e926a23f57f6b9db4dc30a44
Pasquale Dagnello <dagnellopasquale@gmail.com>
2026-05-28 18:17:16 +0200

    Fix: bypassato .env errato sul server forzando Laravel File Manager a usare percorsi relativi (/storage) per le immagini

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('STORAGE_URL', rtrim(env('APP_URL', 'http://localhost'), '/').'/storage'),
+            'url' => '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
